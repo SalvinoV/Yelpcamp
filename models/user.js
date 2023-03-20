9b3da671f917185f6e662ce8de0feaeee3c1ce50
SalvinoV <salvino.varciat@gmail.com>
2023-03-20 16:00:20 -0400

    first commit

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const UserSchema = new Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+});
+// This will add a username, password and salt field to the schema
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
